b5313b17dceceadc750b37206d39e4c13999ba13
isleonpoli <you@example.com>
2025-03-09 05:05:55 -0500

    Agregando contenido

--- src/co.edu.poli.wordshake.model/tutorial.java
+++ src/co.edu.poli.wordshake.model/tutorial.java
@@ -3,5 +3,10 @@ class HolaMundo{
     public static void main(String[] args) {
         System.out.println("Hola mundo estoy probando git_hub");
 
+        int i =0;
+
+        while (i == 2) {
+            i++;
+        }
     }
 }
